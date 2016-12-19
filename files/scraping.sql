dda61b3c970c0405cfd2dafcc04785e8ec65fb73
ghou <ghou@vmware.com>
2016-12-19 10:53:13 +0800

    '添加数据库表'

---
+++ files/scraping.sql
@@ -0,0 +1,18 @@
+﻿# Host: 10.117.171.135  (Version 5.7.16)
+# Date: 2016-12-19 10:51:23
+# Generator: MySQL-Front 5.4  (Build 4.53) - http://www.mysqlfront.de/
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "pages"
+#
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+  `id` bigint(7) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` varchar(10000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ files/wikipedia.sql
@@ -0,0 +1,30 @@
+﻿# Host: 10.117.171.135  (Version 5.7.16)
+# Date: 2016-12-19 10:50:37
+# Generator: MySQL-Front 5.4  (Build 4.53) - http://www.mysqlfront.de/
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "links"
+#
+
+DROP TABLE IF EXISTS `links`;
+CREATE TABLE `links` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fromPageId` int(11) DEFAULT NULL,
+  `toPageId` int(11) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=236 DEFAULT CHARSET=utf8;
+
+#
+# Structure for table "pages"
+#
+
+DROP TABLE IF EXISTS `pages`;
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=230 DEFAULT CHARSET=utf8;
